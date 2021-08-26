83d0185835d38ffbbe739a146b4c801bc42cdcb0
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2021-08-27 02:14:36 +0800

    更新 .gitignore 更新 构建脚本

---
+++ tools/quickLogin/build_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir out
+
+bash ../../build.sh linux amd64 -o ./out/QuickLogin_linux_amd64
+bash ../../build.sh linux arm64 -o ./out/QuickLogin_linux_arm64
+bash ../../build.sh windows amd64 -o ./out/QuickLogin_windows_amd64.exe
+bash ../../build.sh darwin amd64 -o ./out/QuickLogin_darwin_amd64
+bash ../../build.sh darwin arm64 -o ./out/QuickLogin_darwin_arm64
--- tools/quickLogin/go.mod
+++ tools/quickLogin/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/XiaoMengXinX/Fuck163MusicTasks v0.0.0
+	github.com/sirupsen/logrus v1.8.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
